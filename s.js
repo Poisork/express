7ed29cef71e509f748742641b47dbf5ffff6517f
OlegAnn <olegihimich@gmail.com>
2019-03-23 00:42:20 +0200

    static

--- s.js
+++ s.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const app = express();
  // static html 
-// app.use(function (request, response) {
-//   response.sendFile(__dirname + "/index.html");
-// });
+app.use(express.static(__dirname, + '/public'));
 app.get("/",function (request, response) {
   response.send('hello')
 });
 
-app.use("/home",function (request, response) {
-  response.status(404).send(`Ресурс не найден`);
-});
 app.listen(3000);
