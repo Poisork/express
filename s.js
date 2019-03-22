7b3210e2065ec674100409a70c7595728f3d9faa
OlegAnn <olegihimich@gmail.com>
2019-03-23 00:31:27 +0200

    send file

--- s.js
+++ s.js
@@ -1,28 +1,14 @@
-const express = require('express')
-const fs = require('fs')
-const app = express()
-
-app.use((request,response,next) => {
-  let now = new Date();
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
-  let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
-  console.log(data);
-  fs.appendFile("server.log", data + "\n", function(){});
-  next();
-})
-
-app.use("/about", function(request, response, next){
-     
-  console.log("About Middleware");
-  response.send("About Middleware");
+const express = require("express");
+const app = express();
+ // static html 
+// app.use(function (request, response) {
+//   response.sendFile(__dirname + "/index.html");
+// });
+app.get("/",function (request, response) {
+  response.send('hello')
 });
 
-app.get('/', (req,res) => {
-  console.log('core')
-  res.send('<h2>Hello</h2>')
-})
-
-
-app.listen(3000)
+app.use("/home",function (request, response) {
+  response.status(404).send(`Ресурс не найден`);
+});
+app.listen(3000);
